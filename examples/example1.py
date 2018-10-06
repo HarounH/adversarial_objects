019e439eda888e55fc2987fed7cb9ca238539d48
Haroun Habeebe <haroun7@gmail.com>
2018-10-06 00:06:46 -0500

    Created a stop sign in blender that is displayed correctly

--- examples/example1.py
+++ examples/example1.py
@@ -8,7 +8,7 @@ import torch
 import numpy as np
 import tqdm
 import imageio
-
+import pdb
 import neural_renderer as nr
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -20,21 +20,23 @@ def main():
     parser.add_argument('-i', '--filename_input', type=str, default=os.path.join(data_dir, 'teapot.obj'))
     parser.add_argument('-o', '--filename_output', type=str, default=os.path.join(data_dir, 'example1.gif'))
     parser.add_argument('-g', '--gpu', type=int, default=0)
+    parser.add_argument("-lt", "--load_textures", type=bool, default=False, help="Whether or not there is a texture file to be loaded")
+    parser.add_argument('-ts', '--texture_size', type=int, default=4, help="Size of texture file")
     args = parser.parse_args()
 
     # other settings
     camera_distance = 2.732
     elevation = 30
-    texture_size = 2
+    # texture_size = 2
 
     # load .obj
-    vertices, faces = nr.load_obj(args.filename_input)
+    vertices, faces, textures = nr.load_obj(args.filename_input, load_texture=args.load_textures, texture_size=args.texture_size)
     vertices = vertices[None, :, :]  # [num_vertices, XYZ] -> [batch_size=1, num_vertices, XYZ]
     faces = faces[None, :, :]  # [num_faces, 3] -> [batch_size=1, num_faces, 3]
-
+    textures = textures.unsqueeze(0).cuda()
     # create texture [batch_size=1, num_faces, texture_size, texture_size, texture_size, RGB]
-    textures = torch.ones(1, faces.shape[1], texture_size, texture_size, texture_size, 3, dtype=torch.float32).cuda()
-
+    # correct_textures = torch.ones(1, faces.shape[1], texture_size, texture_size, texture_size, 3, dtype=torch.float32).cuda()
+    pdb.set_trace()
     # to gpu
 
     # create renderer
