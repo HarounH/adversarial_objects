634c2cc5c0e203b9be55a9a3403907f4038020d2
Ubuntu <kk20@CS598BO.wgwwj5kw1i4encqukgoeyvtwxh.bx.internal.cloudapp.net>
2018-12-10 04:23:58 +0000

    Adding experiments and a change in the targeted loss function

--- adversarial_objects/experiments.py
+++ adversarial_objects/experiments.py
@@ -25,12 +25,18 @@ def create_cmd(arguments):
 
 
 def varying_regularization():
-	expt_dir = 'varying_regularization_fixed/'
+	expt_dir = 'varying_regularization_largest_w/'
 	output_dir = 'output/{}'.format(expt_dir)
 	mkdir_p('adversarial_objects/'+output_dir)
 	for seed in seeds:
-		base_arguments = '--seed {} --lr 0.005 -iter 500 --adv_tex --adv_ver '.format(seed)
+		base_arguments = '--seed {} --reg_w 0.05 --lr 0.005 -iter 500 --adv_tex --adv_ver '.format(seed)
 		regs = ['surface_area','aabb_volume','radius_volume','edge_length']
+
+		i = 'no_regularization'
+		opdir = output_dir+'/'+i
+		mkdir_p('adversarial_objects/'+opdir)
+		cmd = create_cmd([base_arguments,'--output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{} '.format(seed, opdir, i, seed)])
+		os.system(cmd)
 
 
 		i = "fna_ad_edge_length_nps"
@@ -60,16 +66,94 @@ def varying_regularization():
 		mkdir_p('adversarial_objects/'+opdir)
 		cmd = create_cmd([base_arguments,'--fna_ad --reg edge_length surface_area --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{} '.format(seed, opdir, i, seed)])
 		os.system(cmd)
+
+def varying_nobj():
+	expt_dir = 'varying_nobj/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --lr 0.005 -iter 500 --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+		for j in [1,2,4,8,16]:
+			opdir = output_dir+'/'+str(j)
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--nobj {} --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{}'.format(j, seed, opdir, seed, j)])
+			os.system(cmd)
+
+def varying_training_range():
+	expt_dir = 'varying_training_range/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --lr 0.005 -iter 500 --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+		for j in [35,45]:
+			opdir = output_dir+'/'+str(j)
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--training_range {} --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{}'.format(j, seed, opdir, seed, j)])
+			os.system(cmd)
+
+def varying_ts():
+	expt_dir = 'varying_tex/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --lr 0.005 -iter 500 --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+		for j in [2,3,4,5,6,7,8]:
+			opdir = output_dir+'/'+str(j)
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--ts {} --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{}'.format(j, seed, opdir, seed, j)])
+			os.system(cmd)
+
+def varying_rand_ts():
+	expt_dir = 'varying_rand_tex/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --rng_tex --lr 0.005 -iter 500 --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+		for j in [2,3,4,5,6,7,8]:
+			opdir = output_dir+'/'+str(j)
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--ts {} --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{}'.format(j, seed, opdir, seed, j)])
+			os.system(cmd)
+
+def varying_shapes():
+	expt_dir = 'varying_shapes/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --lr 0.005 -iter 500 --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+		for j in (['obj3.obj', 'obj2.obj', 'obj4.obj','evil_cube_1.obj']):
+			opdir = output_dir+'/'+j
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--attacker_path {} --output {}.png --output_dir {} --tensorboard_dir tensorboard_{}_{}'.format(j, seed, opdir, seed, j)])
+			os.system(cmd)
+
 def varying_targets():
-	base_arguments = '--lr 0.05 -iter 1000 --adv_tex --adv_ver'
-	for x in range(1,43):
-		cmd = create_cmd([base_arguments,'--reg {} --fna_ad --nobj 1 --target_class {} --output targeted_{}.png '.format('edge_length',x,x)])
-		os.system(cmd)
+	expt_dir = 'varying_targets_super_good/'
+	output_dir = 'output/{}'.format(expt_dir)
+	mkdir_p('adversarial_objects/'+output_dir)
+	for seed in seeds:
+		base_arguments = '--seed {} --lr 0.005 -iter 1000 --nobj 2 --ts 4 --training_range 70 --rng_tex --adv_tex --adv_ver --fna_ad --reg edge_length '.format(seed)
+
+		for i in range(42):
+			j = "fna_ad_edge_length_nps"
+			opdir = output_dir+'/'+j
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd([base_arguments,'--nps --output {}_{}_{}.png --target_class {} --output_dir {} --tensorboard_dir tensorboard_{}_{}_{}'.format(seed, j, i, i, opdir, j, seed, i)])
+			os.system(cmd)
+
+		for i in range(42):
+			j = "no_regularization"
+			opdir = output_dir+'/'+j
+			mkdir_p('adversarial_objects/'+opdir)
+			cmd = create_cmd(['--seed {} --lr 0.001 -iter 500 --attacker_path obj4.obj --nobj 2 --ts 4 --rng_tex --adv_tex --adv_ver '.format(seed),' --output {}_{}.png --target_class {} --output_dir {} --tensorboard_dir tensorboard_{}_{}_{}'.format(seed, j, i, opdir, j, seed, i)])
+			os.system(cmd)
 
 
 def basic():
 	base_arguments = '--lr 0.05 -iter 300 --adv_tex --adv_ver'
 	cmd = create_cmd([base_arguments])
 	os.system(cmd)
+
+
 if __name__ == '__main__':
-    varying_regularization()
+	varying_targets()
